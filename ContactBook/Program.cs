3c0573493e8d7e7627a1745c338ea5d19aadcbff
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-08-17 16:32:56 +0530

    Creating new adress book system project

---
+++ ContactBook/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System");         
+        }
+    }
+}
